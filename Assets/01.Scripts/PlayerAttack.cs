2d2a744a441beaffdaee17d90245f31938ba7a5d
Hwaorang <winterer_1@naver.com>
2026-07-13 11:50:21 +0900

    feat : fix

--- Assets/01.Scripts/PlayerAttack.cs
+++ Assets/01.Scripts/PlayerAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] Transform firePosition;
-    [SerializeField] GameObject bullet; // 프리팹으로 만들어서 넣을 예정
+    [SerializeField] GameObject bullet;
     
     [SerializeField] float attackDelay;
     [SerializeField] int level;
@@ -19,7 +19,7 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         if(Keyboard.current.spaceKey.wasPressedThisFrame
-            && Time.time>=nextAttackTime)// 공격 딜레이가 지났는가
+            && Time.time>=nextAttackTime /*공격 딜레이가 지났는가*/)
         {
             Fire(); // 공격
         }
@@ -28,7 +28,7 @@ public class PlayerAttack : MonoBehaviour
     void Fire()
     {
         // 레벨에따른 총알생성
-        //공격다시 사용가능한 시간설정
+        // 공격다시 사용가능한 시간설정
 
         Instantiate(bullet,firePosition.position,Quaternion.identity);
 
